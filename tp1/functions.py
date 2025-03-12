d7735f221b128b6e347722dbbfbdff68becfa0cb
gabichulas <gabylopezromero13@gmail.com>
2025-03-12 13:39:32 -0700

    finish tp1

--- tp1/functions.py
+++ tp1/functions.py
@@ -1,6 +1,6 @@
 import re
 
-path = r"tp1\Tramas_802-15-4.log"
+path = r"redes-2025/tp1/Tramas_802-15-4.log"
 
 with open(path, 'r') as file:
     text = file.read()
@@ -10,3 +10,22 @@ def split_tramas(text):
     tramas = ['7E' + trama for trama in tramas[1:]]
     tramas[-1] = tramas[-1].replace('\n', '')
     return tramas
+
+def calc_checksum(trama):
+    suma = 0
+    res = 0
+    trama_m = trama[6:-2]
+    if "7D7E" in trama_m:
+        trama_m = trama_m.replace("7D7E", "7E")
+    for i in range(0,len(trama_m),2):
+        suma += int(trama_m[i] + trama_m[i + 1], 16)
+    res = int("FF", 16) - (suma % 256)
+    res = format(res, "02x").upper()
+    return res == str(trama[-2:])
+
+def calc_length(trama):
+    length = int(trama[2:6], 16)
+    trama = trama[6:-2]
+    if "7D7E" in trama:
+        trama = trama.replace("7D7E", "7E")
+    return len(trama)/2 == length
---
+++ tp1/main.py
@@ -0,0 +1,44 @@
+from functions import *
+import pandas as pd
+
+path = r"redes-2025/tp1/Tramas_802-15-4.log"
+
+def main():
+    results = []
+    with open(path, 'r') as file:
+        text = file.read()
+    
+    tramas = split_tramas(text)
+    
+    for trama in tramas:
+        longitud_correcta = calc_length(trama)
+        checksum_correcto = calc_checksum(trama)
+        sec_escape = True if "7D7E" in trama else False
+        results.append({
+            "trama": trama,
+            "longitud_correcta": longitud_correcta,
+            "checksum_correcto": checksum_correcto,
+            "sec_escape": sec_escape
+        })
+    
+    df = pd.DataFrame(data=results, columns=["trama", "longitud_correcta", "checksum_correcto", "sec_escape"])
+    
+    print(f"Tramas totales: {len(results)}")
+    print(f"Tramas con longitud correcta: {df["longitud_correcta"].sum()}")
+    print(f"Tramas con longitud incorrecta: {len(results) - df["longitud_correcta"].sum()}")
+    print(f"Tramas con longitud correcta y checksum correcto: {len(df[(df["longitud_correcta"] == True) & (df["checksum_correcto"] == True)])}")
+    print(f"Tramas con longitud correcta y checksum incorrecto: {len(df[(df["longitud_correcta"] == True) & (df["checksum_correcto"] == False)])}")
+    print(f"Secuencias de escape: {df["sec_escape"].sum()}")
+    
+    print("\nLíneas con secuencias de escape:")
+    for i, trama in enumerate(tramas):
+        if "7D7E" in trama:
+            cleaned_trama = trama.replace("7D7E", "7E")
+            print(f"Línea {i}: {cleaned_trama}")
+
+    print("\nLíneas con longitud o checksum incorrecto:")
+    for i, trama in enumerate(tramas):
+        if not df.loc[i, 'longitud_correcta'] or not df.loc[i, 'checksum_correcto']:
+            print(f"Línea {i}: {trama}")
+
+main()
